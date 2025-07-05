3adb0bea6415fbc66e3600932a0614b6549dbe45
Saad R <saadrafiqul93380@gmail.com>
2025-07-05 10:55:18 +0200

    Update package

--- authentication/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-class User(AbstractUser):
-    bio = models.CharField(max_length=150)
-    
-    def __str__(self):
-        return self.username
