c3b2accd713df1e5978c99f2e6e7178c80f03900
Saad R <saadrafiqul93380@gmail.com>
2025-07-10 21:47:16 +0200

    Add models.py

--- niya/company/models.py
+++ niya/company/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100, blank=True, null=True)
+    phone = models.CharField(max_length=100, blank=True, null=True)
+    email = models.CharField(max_length=100, blank=True, null=True)
+    website = models.CharField(max_length=200, blank=True, null=True)
+    description = models.CharField(max_length=500, blank=True, null=True)
+    logo = models.CharField(max_length=500, blank=True, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
