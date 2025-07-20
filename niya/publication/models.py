f624a32077143253c3d7b036d1b9a07b581481d7
Saad R <saadrafiqul93380@gmail.com>
2025-07-20 10:12:40 +0200

    Update models.py

--- niya/publication/models.py
+++ niya/publication/models.py
@@ -6,5 +6,18 @@ User = get_user_model()
 class Publication(models.Model):
     description = models.CharField(max_length=255)
     media = models.CharField(max_length=100, blank=True, null=True)
-    author = models.OneToOneField(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name="liked_comment", blank=True)
+
+    def __str__(self):
+        return self.description
+
+class Comment(models.Model):
+    description = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    publication = models.ForeignKey(Publication, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, related_name="liked_comment", blank=True)
+    def __str__(self):
+        return self.description
