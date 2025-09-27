cfbf7b3109bfcc43e293ff9719b7df993367e73a
Saadiinho <saadrafiqul93380@gmail.com>
2025-09-27 14:11:51 +0200

    Optimize import

--- niya/authentication/tests.py
+++ niya/authentication/tests.py
@@ -1,6 +1,7 @@
-from rest_framework.test import APITestCase
 from django.urls import reverse
 from rest_framework import status
+from rest_framework.test import APITestCase
+
 from .models import User
 
 
