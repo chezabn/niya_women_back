28c2b0db8ff770c343200a6a8138b19f29560283
Saad R <saadrafiqul93380@gmail.com>
2025-12-04 18:59:49 +0100

    Add documentation (Bruno)

--- niya/authentication/urls.py
+++ niya/authentication/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView
 
-from .views import UserAPIView, RegisterAPIView, Healthcheck
-
+from .views import MyUserAPIView, RegisterAPIView, Healthcheck, UsersAPIView
 
 urlpatterns = [
-    path("users/", UserAPIView.as_view(), name="users_api"),
+    path("user/", MyUserAPIView.as_view(), name="my_user_api"),
+    path("users/", UsersAPIView.as_view(), name="users_api"),
     path("register/", RegisterAPIView.as_view(), name="register_api"),
     path("login/", TokenObtainPairView.as_view(), name="login_api"),
     path("healthcheck/", Healthcheck.as_view(), name="healthcheck_auth_api"),
