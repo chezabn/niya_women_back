9306ff89a6f9738c78018e3b43c815df39ab60de
Saad R <saadrafiqul93380@gmail.com>
2025-12-10 19:11:18 +0100

    WIK feature Email verification

--- niya/authentication/models.py
+++ niya/authentication/models.py
@@ -1,9 +1,29 @@
+import random
+from django.utils import timezone
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
     phone = models.IntegerField(null=True, blank=True)
+    email_verified = models.BooleanField(default=False)
+    email_verification_code = models.CharField(max_length=6, null=True, blank=True)
+    email_verification_code_expires = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return self.username
+
+    def generate_verification_code(self):
+        """Génère un code à 6 chiffres valable 15 minutes"""
+        self.email_verification_code = f"{random.randint(100000, 999999)}"
+        self.email_verification_code_expires = timezone.now() + timezone.timedelta(minutes=15)
+        self.save(update_fields=["email_verification_code", "email_verification_code_expires"])
+
+    def is_verification_code_valid(self, code):
+        """Vérifie si le code est correct et non expiré"""
+        if not self.email_verification_code or not self.email_verification_code_expires:
+            return False
+        if timezone.now() > self.email_verification_code_expires:
+            return False
+        return self.email_verification_code == code
--- niya/niya/settings.py
+++ niya/niya/settings.py
@@ -155,3 +155,5 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "authentication.User"
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
