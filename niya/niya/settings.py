288c85f9614db69d69addf190cfd795c2a8ef61c
Saad R <saadrafiqul93380@gmail.com>
2026-03-19 14:19:58 +0100

    Add settings for sending mail

--- niya/niya/settings.py
+++ niya/niya/settings.py
@@ -178,4 +178,10 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "authentication.User"
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "saad.rafiqul1@gmail.com")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "Niyya Women <saad.rafiqul1@gmail.com>")
