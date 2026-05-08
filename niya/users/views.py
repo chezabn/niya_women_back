208fb422fb083e86f82d591aca7d72284d3b0bd1
Saad R <saadrafiqul93380@gmail.com>
2026-05-08 02:02:09 +0200

    Create new app for users logic

---
+++ niya/users/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+class MyUserAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
+
+    def patch(self, request):
+        serializer = UserSerializer(request.user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request):
+        user = request.user
+        user.delete()
+        return Response(
+            {"message": "Account deleted"}, status=status.HTTP_204_NO_CONTENT
+        )
